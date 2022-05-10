ab2c16a2c5d1c74b8d04bf3d9fb11f17c7fc7416
Stephfan052 <104363954+Stephfan052@users.noreply.github.com>
2022-05-10 09:28:03 -0400

    Add files via upload

---
+++ terminal_bot_controller_wireless_encrypted.py
@@ -0,0 +1,49 @@
+# terminal_bot_controller_wireless_encrypted.py
+rom microbit import *
+import radio
+
+def scramble(word, encrypt):
+    alpha = "vLR{}:,'01234 56789ms"
+    crypta = "v9L:3405'R286, 7{}1sm"  
+    result = ''
+    if encrypt is False:
+        temp = alpha
+        alpha = crypta
+        crypta = temp
+
+    for letter in word:
+        index = alpha.find(letter)
+        result = result + crypta[index]
+
+    return result
+
+radio.on()
+radio.config(channel=3,length=64)
+
+sleep(1000)
+
+print("\nSpeeds are -100 to 100\n")
+
+while True:
+    try:
+        vL = int(input("Enter left speed: "))
+        vR = int(input("Enter right speed: "))
+        ms = int(input("Enter ms to run: "))
+
+        dictionary = {  }
+        dictionary['vL'] = vL
+        dictionary['vR'] = vR
+        dictionary['ms'] = ms
+
+        packet = str(dictionary)
+    
+        print("Send: ", packet)
+        packet = scramble(packet, True)
+        print("Encrypted: ", packet)
+        radio.send(packet)
+    
+        print()
+
+    except:
+        print("Error in value entered.")
+        print("Please try again. \n")
